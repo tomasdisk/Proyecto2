29bf388e3481eeaa0157ca8c88d9bcb81aceb475
minigt <minigtraverso@gmail.com>
2017-12-07 13:27:24 -0300

    addedd host config

--- Proyecto/Arduino/final/final.ino
+++ Proyecto/Arduino/final/final.ino
@@ -22,13 +22,11 @@ String data;
 
 String server = "192.168.4.2";
 
-String uri = "/newLog";// our example is /esppost.php
+String uri = "/newLog";
 
 byte dat [5];
 
-
 void setup() {
-
 
   esp.begin(9600);
 
@@ -40,10 +38,11 @@ void setup() {
 
   Serial.println("Test");
   delay(2000);
+
   setWifi();
 }
 
-//reset the esp8266 module
+
 
 void reset() {
 
@@ -56,36 +55,38 @@ void reset() {
 
 }
 
-//connect to your wifi network
-
 void setWifi(){
   esp.println("AT+CWMODE=3");
 
   delay(1000);
 
   if(esp.find("OK") ){
-    Serial.println("Paso 1");
-    esp.println("AT+CWSAP=\"ESP\",\"password\",1,4");
-    if(esp.find("OK") ){
+  Serial.println("Paso 1");
+  esp.println("AT+CWSAP=\"ESP\",\"password\",1,4");
+   if(esp.find("OK") ){
+      Serial.println("Paso 2");
       esp.println("AT+CIPAP=\"192.168.4.1\"");
-      delay(500);
-      if(esp.find("OK") ){
+        delay(500);
+    if(esp.find("OK") ){
+        Serial.println("Paso 3");
         Serial.println("Creado acces point");
         return;
-      }
     }
+   }
   }
+  Serial.println("Fallo setup");
+  setWifi();
 }
 
 void loop () {
 
 // Look for new cards
   if ( ! rfid.PICC_IsNewCardPresent())
-    return;
+  return;
 
   // Verify if the NUID has been readed
   if ( ! rfid.PICC_ReadCardSerial())
-    return;
+  return;
 
   Serial.print(F("PICC type: "));
   MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
@@ -93,29 +94,28 @@ void loop () {
 
   // Check is the PICC of Classic MIFARE type
   if (piccType != MFRC522::PICC_TYPE_MIFARE_MINI &&
-    piccType != MFRC522::PICC_TYPE_MIFARE_1K &&
-    piccType != MFRC522::PICC_TYPE_MIFARE_4K) {
-    Serial.println(F("Your tag is not of type MIFARE Classic."));
-    return;
+  piccType != MFRC522::PICC_TYPE_MIFARE_1K &&
+  piccType != MFRC522::PICC_TYPE_MIFARE_4K) {
+  Serial.println(F("Your tag is not of type MIFARE Classic."));
+  return;
   }
 
   if (rfid.uid.uidByte[0] != nuidPICC[0] ||
-    rfid.uid.uidByte[1] != nuidPICC[1] ||
-    rfid.uid.uidByte[2] != nuidPICC[2] ||
-    rfid.uid.uidByte[3] != nuidPICC[3] ) {
-    Serial.println(F("A new card has been detected."));
+  rfid.uid.uidByte[1] != nuidPICC[1] ||
+  rfid.uid.uidByte[2] != nuidPICC[2] ||
+  rfid.uid.uidByte[3] != nuidPICC[3] ) {
+  Serial.println(F("A new card has been detected."));
 
-    // Store NUID into nuidPICC array
-    data="";
-    for (byte i = 0; i < 4; i++) {
-      nuidPICC[i] = rfid.uid.uidByte[i];
-      data+=nuidPICC[i];
-    }
+  // Store NUID into nuidPICC array
+  data="";
+  for (byte i = 0; i < 4; i++) {
+    nuidPICC[i] = rfid.uid.uidByte[i];
+    data+=nuidPICC[i];
+  }
 
-
-  httppost();
-    Serial.println(F("The NUID tag is:"));
-    Serial.println(data);
+httppost();
+  Serial.println(F("The NUID tag is:"));
+  Serial.println(data);
   }
   else Serial.println(F("Card read previously."));
 
@@ -128,30 +128,22 @@ void loop () {
 
 void httppost () {
 
-
-  //esp.println("AT+CIPSTART=0,\"TCP\",\"google.com\",80");//start a TCP connection.
-  //esp.println("AT+CIPSTART=\"TCP\",\"" + server + "\",3000");//start a TCP connection.
   Serial.println(CIPSTART);
   esp.println(CIPSTART);
   delay(500);
 
   if( esp.find("OK")) {
 
-    Serial.println("TCP connection ready");
+  Serial.println("TCP connection ready");
 
   }
 
-
-  //delay(1000);
-
-  //int l = data.length() + device.length() + 13;
-
   String postRequest =
 
-    "GET " + uri + "?picc=" + data + "&device=" + device + " HTTP/1.1\r\n" +
-    "Host: " + server + "\r\n" +
-    "Connection: close\r\n" +
-    "\r\n";
+  "GET " + uri + "?picc=" + data + "&device=" + device + " HTTP/1.1\r\n" +
+  "Host: " + server + "\r\n" +
+  "Connection: close\r\n" +
+  "\r\n";
 
   Serial.println(postRequest);
 
@@ -162,25 +154,26 @@ void httppost () {
   esp.println(postRequest.length() );
 
   if(esp.find(">")) {
-    Serial.println("Sending.."); esp.print(postRequest);
+  Serial.println("Sending.."); esp.print(postRequest);
 
-    if( esp.find("SEND OK")) {
-      Serial.println("Packet sent");
+  if( esp.find("SEND OK")) {
+    Serial.println("Packet sent");
 
-      while (esp.available()) {
+    while (esp.available()) {
 
-        String tmpResp = esp.readString();
+      String tmpResp = esp.readString();
 
-        Serial.println(tmpResp);
-
-      }
-
-      // close the connection
-
-      esp.println("AT+CIPCLOSE");
+      Serial.println(tmpResp);
 
     }
+
+    // close the connection
+
+    esp.println("AT+CIPCLOSE");
+
+  }
 
   }
   Serial.println("fin post");
 }
+
